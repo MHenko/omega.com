829ae42acf4d7871f772d43f478c3fdd14bd2703
MHenko <55581114+MHenko@users.noreply.github.com>
2020-03-31 14:32:48 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,166 @@
+<?php // Connexion à la base de données
+
+$showCreated = true;
+
+$pdo = new PDO('mysql:host=localhost;dbname=alpha;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+  //vérifie si une image a été envoyée via le formulaire
+  if (isset($_FILES['photo']) && !empty($_FILES['photo'])) {
+    //remplace les caractères spéciaux du nom du fichier par un tiret
+    $photoName = preg_replace( '/[^a-z0-9]+/', '-', strtolower( $_FILES['photo']['name'] ) );
+    //défini le chemin d'enregistrement du fichier
+    $photoPath = "images-articles/$photoName";
+    //récupère le chemin d'enregistrement temporaire de l'image
+    $tmpName = $_FILES['photo']['tmp_name'];
+    //enregistre l'image dans le chemin $photoPath
+    move_uploaded_file($tmpName, $photoPath);
+  }
+
+ if($_POST && ! isset($_POST['delete'])) {
+     $showCreated = true;
+        // Requêtes SQL pour insérer une nouvelle ligne dans la base de données
+        $result = $pdo->prepare('INSERT INTO wear (type, nom, prix, description, matiere, photo) VALUES (:type, :nom, :prix, :description, :matiere, :photo)');
+        // On remplace les éléments préparés par les données envoyées par le formulaire
+        $result->execute(array(
+          'type' => $_POST['type'],
+          'nom' => $_POST['nom'],
+          'prix' => $_POST['prix'],
+          'description' => $_POST['description'],
+          'matiere' => $_POST['matiere'],
+          'photo' => $photoPath
+        ));
+  ?>
+        
+    
+    
+  <?php
+  }
+  ?>
+
+<doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="./image/favicon/favicon.png" />
+
+     <?php include('./includes/styles.php') ?>
+    <link rel="stylesheet" href="css/admin.css">
+   
+    <title>Omega e-commerce</title>
+  </head>
+  <body>
+
+
+<div class="container-fluid">
+  <div class="created-info">L'article a bien été ajouté</div>
+
+  <ul class=" navbar-nav col-12 col-lg-1">
+    <li>
+      <div class="omega1 text-center">
+        <a href="./home.html">
+        <img src="./image/logo/omega.png" alt="logo">
+        </a>
+      </div>
+    </li>
+  </ul>
+
+
+    <div class="col-12 col-lg-10">
+        <div class="col-12 col-1 text-center">
+          <h3> ADMINISTRATEUR</h3>
+        </div>
+
+
+      <div class="container-fluid">
+        <div class=" col-12 col-lg-6 text-center">
+          <label> VETEMENT</label>
+        </div>
+
+         <div class="col-12 col-lg-6 text-center">
+           <input type="text" name="type" >
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <label> NOM</label>
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+           <input type="text" name="nom" >
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <label>PRIX</label>
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <input type="text" name="prix">
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <label>COMPOSITION</label>
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+           <input type="text" name="matiere" >
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <label>IMAGE</label>
+        </div>
+
+        <div class="col-12 col-lg-4 text-center">
+            <input type="text" name="photo">
+        </div>
+
+        <div class="col-12 col-lg-2 text-center">
+           <input type="hidden" name="MAX_FILE_SIZE" value="30000000" />
+          <button type="file" name="photo" accept="image/png, image/jpeg" class="btn1">AJOUT</button>
+        </div>
+
+        <div class="col-12 col-lg-6 text-center">
+          <label> DESCRIPTIF</label>
+
+          <div class="bouton">
+           <button type="submit" class="btn2">ENVOYEZ</button>
+          </div>
+        </div>
+
+        <div class="col-12 col-lg-6">
+             <input class="word" type="text" name="description">
+        </div>
+
+      </div>
+  </div>
+
+    <div class=" col-12 col-lg-1 omega1 text-center">
+      <a href="./home.html">
+        <img src="./image/logo/omega.png" alt="logo">
+      </a>
+    </div>
+</div>
+
+
+
+
+
+<footer>
+</footer>
+
+  </div>
+  </div>
+
+  <?php
+    if($showCreated) { ?>
+        <script>
+          let createdInfo = document.querySelector('.created-info');
+          createdInfo.classList.add('show');
+          setTimeout(function() {
+            createdInfo.classList.remove('show');
+          }, 4000);
+        </script>
+      
+<?php } ?>
+  </body>
+</html>

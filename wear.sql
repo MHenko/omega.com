7f1780abd7d47d7545efe3e8e043ce5fadd1c2a2
M-L-H3nk0 <55581114+M-L-H3nk0@users.noreply.github.com>
2020-05-05 12:08:00 +0200

    Update wear.sql

--- wear.sql
+++ wear.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 21 avr. 2020 à 09:16
+-- Généré le : mar. 05 mai 2020 à 12:07
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.3.15
 
@@ -48,7 +48,55 @@ INSERT INTO `wear` (`id`, `type`, `nom`, `prix`, `description`, `matiere`, `phot
 (21, 'TSHIRT', 'NOM', '150$', '', 'tout et rien', 'images-articles/4.jpg'),
 (22, 'TSHIRT', '102', '', '', '30% polystere', 'images-articles/3.jpg'),
 (25, 'TSHIRT', 'ertyuiop', '120£', '', '30% polystere', 'images-articles/4.jpg'),
-(26, 'TSHIRT', 'Largent', '150$', '', 'tout et rien', 'images-articles/1.jpg');
+(26, 'TSHIRT', 'Largent', '150$', '', 'tout et rien', 'images-articles/1.jpg'),
+(27, 'CHEMISE', 'ertyuiop', '150$', '', '30% polystere', 'images-articles/5.jpg'),
+(28, 'CHEMISE', 'MAO', '150$', '', 'tout et rien', 'images-articles/6.jpg'),
+(29, 'CHEMISE', '103', '150$', '', '30% polystere', 'images-articles/7.jpg'),
+(30, 'CHEMISE', 'OAM', '120£', '', '30% polystere', 'images-articles/8.jpg'),
+(31, 'CHEMISE', 'FATIGUE', '150$', '', 'tout et rien', 'images-articles/9.jpg'),
+(44, 'VESTE', '102', '150$', '', '30% polystere', 'images-articles/16.jpg'),
+(45, 'VESTE', 'MAO', '150$', '', 'tout et rien', 'images-articles/17.jpg'),
+(46, 'VESTE', 'NOM', '120£', '', '30% polystere', 'images-articles/18.jpg'),
+(48, 'VESTE', 'Largent ', '120£', '', 'tout et rien', 'images-articles/21.jpg'),
+(49, 'VESTE', 'MAO', '120£', '', '30% polystere', 'images-articles/20.jpg'),
+(50, 'VESTE', 'Largent', '150$', '', 'tout et rien', 'images-articles/21.jpg'),
+(51, 'VESTE', '102', '120£', '', 'tout et rien', 'images-articles/22.jpg'),
+(52, 'MANTEAU', 'MAO', '150$', '', '30% polystere', 'images-articles/24.jpg'),
+(53, 'MANTEAU', 'NOM', '120£', '', 'tout et rien', 'images-articles/25.jpg'),
+(54, 'MANTEAU', 'MAO', '120£', '', 'tout et rien', 'images-articles/26.jpg'),
+(55, 'MANTEAU', 'MAO', '120£', '', '30% polystere', 'images-articles/27.jpg'),
+(56, 'MANTEAU', 'MAO', '150$', '', '30% polystere', 'images-articles/28.jpg'),
+(58, 'MANTEAU', 'MAO', '120£', '', 'tout et rien', 'images-articles/29.jpg'),
+(59, 'SWEAT', 'NOM', '150$', '', '30% polystere', 'images-articles/10.jpg'),
+(60, 'SWEAT', '102', '150$', '', '30% polystere', 'images-articles/11.jpg'),
+(61, 'SWEAT', '102', '120£', '', 'tout et rien', 'images-articles/12.jpg'),
+(62, 'SWEAT', 'NOM', '120£', '', '30% polystere', 'images-articles/13.jpg'),
+(63, 'SWEAT', 'Largent', '120£', '', 'tout et rien', 'images-articles/14.jpg'),
+(64, 'SWEAT', 'Largent ', '150$', '', 'tout et rien', 'images-articles/15.jpg'),
+(65, 'PANTALON', 'NOM', '120£', '', 'tout et rien', 'images-articles/30.jpg'),
+(66, 'PANTALON', 'NOM', '150$', '', '30% polystere', 'images-articles/31.jpg'),
+(67, 'PANTALON', 'MAO', '120£', '', '30% polystere', 'images-articles/32.jpg'),
+(68, 'PANTALON', 'MAO', '150$', '', '30% polystere', 'images-articles/33.jpg'),
+(69, 'PANTALON', '102', '150$', '', '30% polystere', 'images-articles/34.jpg'),
+(70, 'PANTALON', 'Largent ', '150$', '', '30% polystere', 'images-articles/35.jpg'),
+(71, 'CHAUSSURE', 'MAO', '150$', '', '30% polystere', 'images-articles/36.jpg'),
+(72, 'CHAUSSURE', 'MAO', '120£', '', 'tout et rien', 'images-articles/37.jpg'),
+(73, 'CHAUSSURE', 'MAO', '150$', '', '30% polystere', 'images-articles/38.jpg'),
+(74, 'CHAUSSURE', 'NOM', '120£', '', 'tout et rien', 'images-articles/39.jpg'),
+(75, 'CHAUSSURE', 'NOM', '120£', '', 'tout et rien', 'images-articles/40.jpg'),
+(76, 'CHAUSSURE', 'ertyuiop', '150$', '', 'tout et rien', 'images-articles/41.jpg'),
+(77, 'SAC', 'MAO', '120£', '', 'tout et rien', 'images-articles/42.jpg'),
+(78, 'SAC', '102', '150$', '', '30% polystere', 'images-articles/43.jpg'),
+(79, 'SAC', 'NOM', '120£', '', '30% polystere', 'images-articles/44.jpg'),
+(80, 'SAC', 'Largent', '150$', '', 'tout et rien', 'images-articles/45.jpg'),
+(82, 'SAC', '102', '150$', '', '30% polystere', 'images-articles/46.jpg'),
+(83, 'SAC', 'Largent', '120£', '', '30% polystere', 'images-articles/47.jpg'),
+(84, 'ACCESSOIRE', 'MAO', '120£', '', '30% polystere', 'images-articles/48.jpg'),
+(85, 'ACCESSOIRE', 'NOM', '150$', '', '30% polystere', 'images-articles/49.jpg'),
+(86, 'ACCESSOIRE', 'NOM', '120£', '', '30% polystere', 'images-articles/50.jpg'),
+(87, 'ACCESSOIRE', 'MAO', '150$', '', 'tout et rien', 'images-articles/51.jpg'),
+(88, 'ACCESSOIRE', 'NOM', '150$', '', 'tout et rien', 'images-articles/52.jpg'),
+(89, 'ACCESSOIRE', 'MAO', '120£', '', 'tout et rien', 'images-articles/53.jpg');
 
 --
 -- Index pour les tables déchargées
@@ -68,7 +116,7 @@ ALTER TABLE `wear`
 -- AUTO_INCREMENT pour la table `wear`
 --
 ALTER TABLE `wear`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
